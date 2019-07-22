676ff708a9510823c1608eac8b1fcf1dff75a03b
Celso Katsuhiro Kushi <celsok@cit>
2019-07-22 11:42:16 -0300

    ajustes

--- src/br/com/ciandt/ModificadoresDeAcesso.java
+++ src/br/com/ciandt/ModificadoresDeAcesso.java
@@ -1,12 +1,10 @@
 package br.com.ciandt;
 
-import br.com.ciandt.models.Pessoa;
-
 public class ModificadoresDeAcesso {
 
 	public static void main(String[] args) {
 		
-		Pessoa joao = new Pessoa();
+		//Crie a variável joao usando a classe Pessoa
 		
 	}
 }
