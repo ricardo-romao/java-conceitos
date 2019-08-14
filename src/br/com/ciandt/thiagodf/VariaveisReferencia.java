f6699bee119a684dd71a9a1cc8116ec98d5cfd1f
thiagodf <thiagodf@ciandt.com>
2019-08-14 13:26:23 -0300

    Corrigindo erro de implementação

--- src/br/com/ciandt/thiagodf/VariaveisReferencia.java
+++ src/br/com/ciandt/thiagodf/VariaveisReferencia.java
@@ -2,17 +2,28 @@ package br.com.ciandt.thiagodf;
 
 import br.com.ciandt.thiagodf.models.Pessoa;
 
+/**
+ * Classe de VariaveisReferencia.
+ * 
+ * @author thiagodf
+ *
+ */
 public class VariaveisReferencia {
-
+	
+	/**
+	 * Método que intancia a classe pessoa
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		Pessoa joao = new Pessoa();
-		joao.setNome("Jo�o");
+		joao.setNome("João");
 		
-		Pessoa pedro = joao;
+		Pessoa pedro = new Pessoa();
 		pedro.setNome("Pedro");
 		
-		System.out.println("O nome do Jo�o � " + joao.getNome());
-		System.out.println("O nome do Pedro � " + pedro.getNome());
+		System.out.println("O nome do João ? " + joao.getNome());
+		System.out.println("O nome do Pedro ? " + pedro.getNome());
 	}
 }
